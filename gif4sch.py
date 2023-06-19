e89f4e82049fe0ffa60817d260dd91027d72cc73
valeria-sevostyanova <125126489+ValeriaSevostyanova@users.noreply.github.com>
2023-06-19 11:51:23 +0300

    Add files via upload

---
+++ gif4sch.py
@@ -0,0 +1,31 @@
+from kivy.app import App
+from kivy.uix.image import Image
+from kivy.core.window import Window
+
+Window.size = (600, 400)
+# (Window.width, Window.height)
+Window.borderless = 1
+
+class MyApp(App):
+    def build(self):
+        i = Image(source='кот-чавкает.gif',
+                  size_hint=(Window.width, Window.height),
+                  pos_hint={'center_x': .5, 'center_y': .5})
+        return i
+
+if __name__ == '__main__':
+    app = MyApp()
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
